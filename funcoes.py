abd8705bf6e3e7276dd84c52a79068b69dbf243b
annalba-insper <annalba@al.insper.edu.br>
2026-04-17 08:56:01 -0300

    Question 1: Rolar dados

---
+++ funcoes.py
@@ -0,0 +1,10 @@
+import random
+
+def rolar_dados(numero_dados):
+    lista = []
+    i = 0
+    while i < numero_dados:
+        resultado = random.randint(1, 6)
+        lista.append(resultado)
+        i += 1
+    return lista
