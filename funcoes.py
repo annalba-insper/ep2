e5f1e0411e6ff45e3813b73c178a05ccfe932d55
annalba-insper <annalba@al.insper.edu.br>
2026-04-17 09:36:40 -0300

    Question 2: Guardar dado

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,22 @@ def rolar_dados(numero_dados):
         lista.append(resultado)
         i += 1
     return lista
+
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dados_rolados_final = [] 
+    valor_guardado = dados_rolados[dado_para_guardar]
+
+    i = 0
+    while i < len(dados_rolados):
+        if i != dado_para_guardar:
+            dados_rolados_final.append(dados_rolados[i])
+        i+=1
+
+    dados_no_estoque.append(valor_guardado)  
+
+    return [dados_rolados_final, dados_no_estoque]    
+
+dados_rolados = [6, 1, 6, 4]
+dados_no_estoque = [2]
+dado_para_guardar = 2
