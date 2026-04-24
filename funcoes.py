dd6a032c47da73459b0e3d4a5a4126fa8bdb6414
annalba-insper <annalba@al.insper.edu.br>
2026-04-24 16:21:22 -0300

    Question 4: Calcula pontos na regra simples

--- funcoes.py
+++ funcoes.py
@@ -30,3 +30,15 @@ def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
         del dados_no_estoque[dado_para_remover]
     
     return [dados_rolados, dados_no_estoque]
+
+def calcula_pontos_regra_simples(dados_rolados):
+    dicionario_pontos = {}
+
+    for numero in range(1,7):
+        if numero not in dicionario_pontos:
+            dicionario_pontos[numero] = 0
+
+    for i in range(0, len(dados_rolados)):
+        dicionario_pontos[dados_rolados[i]] += dados_rolados[i]
+
+    return dicionario_pontos
